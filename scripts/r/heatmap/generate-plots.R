aa26438337edf76cd70ec5027e0bf50b1b74c0c0
Kornel Kielczewski <kornel.kielczewski@allegrogroup.com>
2016-03-04 20:20:04 +0100

    issue #6: Clustering with nas

--- scripts/r/heatmap/generate-plots.R
+++ scripts/r/heatmap/generate-plots.R
@@ -4,7 +4,6 @@ library(RColorBrewer)
 library(reshape2)
 
 heatmap.data <- read.csv('../../results/heatmap/heatmap-data.csv')
-
 h <- heatmap.data[, c('log2median', 'log2mean', 'Gene.Id', 'tumor.name')]  
 
 plot.heatmap <- function(which, h, threshold, filename) {
@@ -15,11 +14,22 @@ plot.heatmap <- function(which, h, threshold, filename) {
   h.wide$tumor.name <- NULL
   h.wide <- t(h.wide)
   h.wide <- data.frame(h.wide)
-  h.wide <- h.wide[complete.cases(h.wide),]
-  pheatmap(h.wide[abs(rowMeans(h.wide)) >= threshold, ], 
+  #h.wide <- h.wide[complete.cases(h.wide),]
+  
+  f <- h.wide[abs(rowMeans(h.wide, na.rm = T)) >= threshold,]
+  
+  cluster.rows <- f
+  cluster.rows[is.na(cluster.rows)] <- 10
+  cluster.rows <- hclust(dist(cluster.rows))
+  
+  cluster.cols <- t(f)
+  cluster.cols[is.na(cluster.cols)] <- 10
+  cluster.cols <- hclust(dist(cluster.cols))
+  
+  pheatmap(f, 
            show_colnames = T, 
-           cluster_cols = T, 
-           cluster_rows = T, 
+           cluster_cols = cluster.cols, 
+           cluster_rows = cluster.rows, 
            fontsize_row = 10,
            filename = filename,
            cellwidth = 10,
@@ -37,3 +47,4 @@ for (threshold in seq(0, 1, 0.1)) {
   plot.heatmap('mean', h, threshold, sprintf('%s/%f-heatmap-mean.png', results.dir, threshold))
   plot.heatmap('median', h, threshold, sprintf('%s/%f-heatmap-median.png', results.dir, threshold))
 }
+
