daddb397b62f22c09a16524e042c50a697f27b0a
Kornel Kielczewski <kornel.kielczewski@allegrogroup.com>
2016-03-07 22:32:24 +0100

    wip

--- scripts/r/testDESSeq2.R
+++ scripts/r/testDESSeq2.R
@@ -5,7 +5,6 @@ library(reshape2)
 library(data.table)
 library(pheatmap)
 
-
 genes <- read.csv('../../data/KRAB ZNF gene master list.csv', sep = '\t')
 genes$Gene.ID <- as.character(genes$Gene.ID)
 
@@ -32,48 +31,70 @@ genes$Gene.ID <- as.character(genes$Gene.ID)
 source('utils.R')
 
 
+file <- filtered.files[1]
+
 for (file in filtered.files) {
   tumor.name <- sub('gdac.broadinstitute.org_(\\w*)\\..*', '\\1', file)
   
   print(tumor.name)
   
-  x <- read_delim(sprintf('%s/%s', files.path, file), delim = '\t')
+  rawdata <- read_delim(sprintf('%s/%s', files.path, file), delim = '\t')
   
-  x2 <- x[,seq(2, length(x), 3)]
+  raw.counts <- rawdata[,seq(2, length(rawdata), 3)]
   
-  x2$HybRefShort <- sub('\\|.*', '', x$`Hybridization REF`)
+  raw.counts$HybRefShort <- sub('\\|.*', '', rawdata$`Hybridization REF`)
   
-  x2 <- genes %>% inner_join(x2, by = c('Gene.ID' = 'HybRefShort'))
-  x2$HybRefShort <- NULL
-  x2$Ensembl.ID <- NULL
-  x2$`Hybridization REF` <- NULL
+  selected <- genes %>% inner_join(raw.counts, by = c('Gene.ID' = 'HybRefShort'))
+  selected$HybRefShort <- NULL
+  selected$Ensembl.ID <- NULL
+  selected$`Hybridization REF` <- NULL
   
-  rownames(x2) <- x2$Gene.ID
+  rownames(selected) <- selected$Gene.ID
   
-  x2$Gene.ID <- NULL
+  selected$Gene.ID <- NULL
   
-  x3 <- as.data.frame(sapply(x2, as.integer))
-  rownames(x3) <- rownames(x2)
+  counts <- as.data.frame(sapply(selected, as.integer))
+  rownames(counts) <- rownames(selected)
   
-  condition <- factor(barcode.to.type(colnames(x3)))
+  condition <- factor(barcode.to.type(colnames(counts)))
   
-  count2 <- x3[,condition != 'other']
-  cond2 <- factor(barcode.to.type(colnames(count2)))
-  cds <- newCountDataSet(count2, cond2)
+  counts <- counts[,condition != 'other']
+  conditions <- factor(barcode.to.type(colnames(counts)))
+  cds <- newCountDataSet(counts, conditions)
+  
   cds <- estimateSizeFactors(cds)
-  
   cds <- estimateDispersions(cds)
   #plotDispEsts(cds)
   
+  stats <- counts
+  colnames(stats) <- unlist(barcode.to.type(colnames(stats)))
+  stats.healthy <- stats[,grep('healthy', colnames(stats))]
+  stats.tumor <- stats[,grep('tumor', colnames(stats))]
+  
+  stats$medianHealthy <- apply(stats.healthy, 1, median)
+  stats$medianTumor <- apply(stats.tumor, 1, median)
+  
+  stats$IQRhealthy <- apply(stats.healthy, 1, IQR)
+  stats$IQRtumor <- apply(stats.tumor, 1, IQR)
+  
+  stats$SEhealthy <- apply(stats.healthy, 1, se)
+  stats$SEtumor <- apply(stats.tumor, 1, se)
+  
+  stats$Gene.ID <- rownames(stats)
+  
+  stats <- subset(stats, select = c('Gene.ID', 'medianHealthy', 'medianTumor', 'IQRhealthy', 'IQRtumor', 'SEhealthy', 'SEtumor'))
+  
   res <- nbinomTest(cds, 'healthy', 'tumor')
+  
+  res <- res %>% inner_join(stats, by = c('id' = 'Gene.ID'))
+  res <- rename(res, c('baseMeanA' = 'baseMeanHealthy', 'baseMeanB' = 'baseMeanTumor'))
+  
   write.csv(res, file = sprintf('../../results/bionmialTest/%s.csv', tumor.name), row.names = F)
 }
 
 files <- list.files(path = '../../results/bionmialTest', pattern = '*.csv', recursive = T, full.names = T)
 
 all <- data.frame()
-
-
 
 for (file in files) {
   tumor.name <- sub('.*/(\\w*)\\.csv', '\\1', file)
@@ -96,21 +117,3 @@ sorted <- all.2[,cnames]
 
 write.csv(all, file = '../../results/bionmialTest/heatmap-data.csv', row.names = F)
 write.csv(sorted, file = '../../results/bionmialTest/full-table-pvalues.csv', row.names = F)
-
-heatmap.data <- read.csv('../../results/bionmialTest/heatmap-data.csv')
-
-h <- heatmap.data[, c('tumor', 'id', 'log2FoldChange')]  
-
-h.mean <- dcast(h, tumor ~ id, value.var = 'log2FoldChange')
-rownames(h.mean) <- h.mean$tumor.name
-h.mean$tumor.name <- NULL
-h.mean <- t(h.mean)
-pheatmap(h.mean, 
-         show_colnames = T, 
-         cluster_cols = T, 
-         cluster_rows = T, 
-         fontsize_row = 2,
-         #   filename = sprintf('%s/heatmap-mean.png', results.dir),
-         width = 10,
-         height = 20)
-
