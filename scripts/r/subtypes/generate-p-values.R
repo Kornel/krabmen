dbc946d55b2e89e79f97778232dec12e6a5035ab
Kornel Kielczewski <kornel.kielczewski@allegrogroup.com>
2016-05-14 21:06:07 +0200

    Subtypes for each column

--- scripts/r/subtypes/generate-p-values.R
+++ scripts/r/subtypes/generate-p-values.R
@@ -1,51 +1,13 @@
 source('subtypes/utils.R')
 
-# PRAD
-t.f <- '../../download/mrna-rsem-normalized-2015-11-01/gdac.broadinstitute.org_PRAD.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3.2015110100.0.0/PRAD.rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.data.txt'
-data <- load.tumor(t.f)
+subtypes.dir <- '../../KRAB_and_TCGA_Subtypes/'
+mrna.dir <- '../../download/mrna-rsem-normalized-2015-11-01/'
 
-s.f <- '../../data/PRAD.clinical.tsv'
-o.f <- '../../subtypes/result-PRAD.clinical.csv'
-generate.pvalues(data, s.f, o.f)
-
-s.f <- '../../data/Table_S1_PRAD.csv'
-o.f <- '../../subtypes/result-TABLE_S1_PRAD.csv'
-generate.pvalues(data, s.f, o.f)
-
-# THCA
-
-data <- load.tumor('../../download/mrna-rsem-normalized-2015-11-01/gdac.broadinstitute.org_THCA.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3.2015110100.0.0/THCA.rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.data.txt')
-
-s.f <- '../../data/THCA.clinical.tsv'
-o.f <- '../../subtypes/result-THCA.clinical.csv'
-generate.pvalues(data, s.f, o.f)
-
-s.f <- '../../data/THCA_RNA_Subtypes.tsv'
-o.f <- '../../subtypes/result-THCA_RNA_Subtypes.csv'
-generate.pvalues(data, s.f, o.f)
-
-# BRCA
-
-data <- load.tumor('../../download/mrna-rsem-normalized-2015-11-01/gdac.broadinstitute.org_BRCA.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3.2015110100.0.0/BRCA.rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.data.txt')
-s.f <- '../../data/subtypes_BRCA.tsv'
-o.f <- '../../subtypes/result-subtypes_BRCA.csv'
-generate.pvalues(data, s.f, o.f)
-
-# BLCA
-
-data <- load.tumor('../../download/mrna-rsem-normalized-2015-11-01/gdac.broadinstitute.org_BLCA.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3.2015110100.0.0/BLCA.rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.data.txt')
-s.f <- '../../data/BLCA.clinical.tsv'
-o.f <- '../../subtypes/result-BLCA.clinical.csv'
-generate.pvalues(data, s.f, o.f)
-
-s.f <- '../../data/BLCA_mRNA_Clusters.tsv'
-o.f <- '../../subtypes/result-BLCA_mRNA_Clusters.csv'
-generate.pvalues(data, s.f, o.f)
-
-# LUSC
-
-data <- load.tumor('../../download/mrna-rsem-normalized-2015-11-01/gdac.broadinstitute.org_LUSC.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3.2015110100.0.0/LUSC.rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.data.txt')
-s.f <- '../../data/LUSC.clinical.basic.tsv'
-o.f <- '../../subtypes/result-LUSC.clinical.basic.csv'
-generate.pvalues(data, s.f, o.f)
-
+for (subtype.file in list.files(subtypes.dir, pattern = 'csv', full.names = F)) {
+  tumor <- gsub('([a-zA-Z]+).*', '\\1', subtype.file)
+  subtype.full.file <- file.path(subtypes.dir, subtype.file)
+  
+  tumor.file <- list.files(mrna.dir, pattern = tumor, recursive = T, full.names = T)
+  
+  if (length(tumor.file) > 0) compare.subtypes(tumor.file, subtype.full.file)
+}
--- scripts/r/subtypes/utils.R
+++ scripts/r/subtypes/utils.R
@@ -1,7 +1,12 @@
 library(dplyr)
+library(readr)
 source('data-utils.R')
+library(reshape2)
 
-load.tumor <- function(path) {
+.load.tumor <- function(path) {
+  
+  print(paste('Loading tumor data from', path))
+  
   prad <- read_delim(path, delim = '\t')
   
   raw.counts <- prad[-1,]
@@ -16,29 +21,62 @@ load.tumor <- function(path) {
   rownames(data) <- r
   data <- as.data.frame(t(data))
   data$barcode <- substr(rownames(data), 1, 15)
+  data <- data %>% filter(substr(barcode, 14, 15) == '01')
+  data$barcode <- substr(data$barcode, 1, 12)
   data
 }
 
-generate.pvalues <- function(data, subtypes.file, output) {
+.compare.subtypes <- function(subtypes.file, data) {
   
-  subtype.mapping <- read.csv('../../data/LUSC.clinical.basic.tsv', sep = '\t', stringsAsFactors = F, col.names = c('barcode', 'subtype')) %>% filter(nchar(subtype) > 0)
-  subtype.mapping$subtype <- as.character(subtype.mapping$subtype)
-  gene.list <- colnames(data)[-length(colnames(data))]
+  genes <- setdiff(colnames(data), 'barcode')
   
-  subtype.data <- data %>% inner_join(subtype.mapping, by = c('barcode' = 'barcode'))
+  subtypes.file.name <- gsub('.*/(\\w+).csv', '\\1', subtypes.file)
   
-  if (length(unique(subtype.data$subtype)) == 1) {
-    print(sprintf('Cannot perform comparison, only one subtype found %s', toString(unique(subtype.data$subtype))))
-  } else {
-    b <- data.frame()
-    for (gene in gene.list) {
-      a <- aov(subtype.data[,gene] ~ subtype.data$subtype)
-      t.hsd <- as.data.frame(TukeyHSD(a)$`subtype.data$subtype`)
-      t.hsd$type <- rownames(t.hsd)
-      t.hsd$gene <- gene
-      b <- rbind(b, dcast(t.hsd, gene ~ type, value.var = c('p adj')))
-    }
+  subtypes.data <- read_delim(subtypes.file, delim = ';', na = c('NA', 'na', ''))
+  
+  join <- data %>% inner_join(subtypes.data, by = c('barcode' = 'sampleID'))
+  
+  subtypes <- setdiff(colnames(join), c(genes, 'barcode'))
+  
+  ignored <- c('age', 'OS_IND', 'OS_days', 'days_to_death', 'vital_status', 'days_to_last_followup', 
+               'RFS_days', 'RFS_IND', 'TIME_TO_EVENT', 'days_to_new_tumor', 'number_pack_years_smoked', 
+               'OS_months', 'RFS_months', 'Age', 'Days.to.Last.Followup')
+  
+  subtypes <- setdiff(subtypes, ignored)
+  
+  for (subtype in subtypes) {
     
-    write.table(b, file = output, row.names = F, sep = ',')
+    na <- is.na(join[,subtype])
+    
+    subtCol <- as.character(join[,subtype])[!na]
+    
+    subtypes.str <- toString(paste(unlist(unique(subtCol)), collapse = ', '))
+    
+    if (length(unique(subtCol)) <= 1) {
+      print(sprintf('Cannot perform comparison, only values of %s found: %s', subtype, subtypes.str))
+    } else {
+      print(sprintf('For %s found: %s', subtype, subtypes.str))
+      
+      dir <- file.path('../../KRAB_and_TCGA_Subtypes/results', subtypes.file.name)
+      dir.create(dir, showWarnings = F, recursive = T)
+      file <- file.path(dir, paste0(subtype, '.csv'))
+      
+      b <- data.frame()
+      for (gene in genes) {
+        
+        geneCol <- join[,gene][!na]
+        a <- aov(geneCol ~ subtCol)
+        t.hsd <- as.data.frame(TukeyHSD(a)$`subtCol`)
+        t.hsd$type <- rownames(t.hsd)
+        t.hsd$gene <- gene
+        b <- rbind(b, dcast(t.hsd, gene ~ type, value.var = c('p adj')))
+      }
+      write.table(b, file = file, row.names = F, sep = ',')
+    }
   }
+}
+
+compare.subtypes <- function(tumor.file, subtypes.file) {
+  data <- .load.tumor(tumor.file)
+  .compare.subtypes(subtypes.file, data)
 }
